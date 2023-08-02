fcf8e334a46b5a272866d2425f81e10be4c008d9
trunow <s@trnw.ru>
2023-08-02 19:10:06 +0300

    fix baseUri

--- front/src/main.js
+++ front/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
-const baseUri = 'http://127.0.0.1:3003';//window?.location?.origin;
+const origin = window?.location?.origin;
+
+const baseUri = origin==='https://practicum.trunow.ru'?origin:'http://127.0.0.1:3003';
 const apiUrl =  [baseUri, 'me'].join('/');
 const isProd = baseUri === 'https://practicum.trunow.ru';
 const token = window.localStorage.getItem('token');
